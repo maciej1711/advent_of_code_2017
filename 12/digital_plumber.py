c0e44c9549bd018bd781b9c5979366875e6e0858
Macias <maciej1711@gmail.com>
2017-12-13 13:46:33 +0100

    Day 12 - Advent of Code 2017
    Part 1 of the digital plumber algorythm for Advent of Code 2017.

--- 12/digital_plumber.py
+++ 12/digital_plumber.py
@@ -1,35 +1,41 @@
 import argparse
+from collections import defaultdict
 """
---- Day 11: Hex Ed ---
+--- Day 12: Digital Plumber ---
 
-Crossing the bridge, you've barely reached the other side of the stream when a program comes up to you, clearly in distress. "It's my child process," she says, "he's gotten lost in an infinite grid!"
+Walking along the memory banks of the stream, you find a small village that is experiencing a little confusion: some programs can't communicate with each other.
 
-Fortunately for her, you have plenty of experience with infinite grids.
+Programs in this village communicate using a fixed system of pipes. Messages are passed between programs using these pipes, but most programs aren't connected to each other directly. Instead, programs pass messages between each other until the message reaches the intended recipient.
 
-Unfortunately for you, it's a hex grid.
+For some reason, though, some of these messages aren't ever reaching their intended recipient, and the programs suspect that some pipes are missing. They would like you to investigate.
 
-The hexagons ("hexes") in this grid are aligned such that adjacent hexes can be found to the north, northeast, southeast, south, southwest, and northwest:
+You walk through the village and record the ID of each program and the IDs with which it can communicate directly (your puzzle input). Each program has one or more programs with which it can communicate, and these pipes are bidirectional; if 8 says it can communicate with 11, then 11 will say it can communicate with 8.
 
-  \ n  /
-nw +--+ ne
-  /    \
--+      +-
-  \    /
-sw +--+ se
-  / s  \
+You need to figure out how many programs are in the group that contains program ID 0.
 
-You have the path the child process took. Starting where he started, you need to determine the fewest number of steps required to reach him. (A "step" means to move from the hex you are in to any adjacent hex.)
+For example, suppose you go door-to-door like a travelling salesman and record the following list:
 
-For example:
+0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5
 
-    ne,ne,ne is 3 steps away.
-    ne,ne,sw,sw is 0 steps away (back where you started).
-    ne,ne,s,s is 2 steps away (se,se).
-    se,sw,se,sw,sw is 3 steps away (s,s,sw).
+In this example, the following programs are in the group that contains program ID 0:
 
---- Part Two ---
+    Program 0 by definition.
+    Program 2, directly connected to program 0.
+    Program 3 via program 2.
+    Program 4 via program 2.
+    Program 5 via programs 6, then 4, then 2.
+    Program 6 via programs 4, then 2.
 
-How many steps away is the furthest he ever got from his starting position?
+Therefore, a total of 6 programs are in this group; all but program 1, which has a pipe that connects it to itself.
+
+How many programs are in the group that contains program ID 0?
+
 
 """
 
@@ -39,44 +45,55 @@ def open_file(file_name):
     Opens a file and returns its content as a list
     '''
     with open(file_name[0], 'r') as input_file:
-        content = input_file.read().split(",")
+        content = input_file.readlines()
     return content
 
 
-def follow_the_path(file_name):
+def interconnected_groups(file_name):
     """
-    Given a path, this function follows direction and calculates how far from starting hex field it went
+    This program iterates over lines connected with group {0} and append all of the possible groups
+    to a set collection to avoid duplicated entries.
     :param file_name: name of a file with input data
+    :return:
     """
-    # Calculate cube distance as per: https://www.redblobgames.com/grids/hexagons/ Distances
-    path = open_file(file_name)
-    max_distance = 0
-    # Set starting position
-    position = (0, 0)
-    directions = {"n": (0, 1),
-            "s": (0, -1),
-            "ne": (1, 0),
-            "sw": (-1, 0),
-            "nw": (-1, 1),
-            "se": (1, -1), }
-    for item in path:
-        # Change position with each move
-        position = (position[0] + directions[item][0], position[1] + directions[item][1])
-        max_distance = max(max_distance, max(abs(position[0]), abs(position[1]), abs(position[0]+position[1])))
+    content = open_file(file_name)
+    content_dict = defaultdict(list)
 
-    print "Shortest cube distance is: %i" % max(abs(position[0]), abs(position[1]), abs(position[0]+position[1]))
-    print "The furthest distance was: %i steps" % max_distance
+    # Create defautl dict with all the data provided in input file
+    for line in content:
+        line_items = line.split()
+        line_id = int(line_items[0])
+        line_communicate_ids = map(lambda x: int(x.strip(',')), line_items[2:])
+        # Create content dict
+        for id in line_communicate_ids:
+            # Assign all the values to the line_id
+            content_dict[line_id].append(id)
+            # Assign all connected values to the connected items in the line
+            content_dict[id].append(line_id)
+
+    search_group = [0]
+    # Initialize collection with no duplicated entries
+    set_collection = set()
+    while search_group:
+        # Remove item from search group so it won't duplicate
+        removed_item = search_group.pop()
+        # Check for the removed item in the content default dict
+        for item_line in content_dict[removed_item]:
+            # If the item is not in the set collection - add it. Add the value from item_line to the search group.
+            if item_line not in set_collection:
+                set_collection.add(item_line)
+                search_group.append(item_line)
+    print "Number of programs with same group as program {0} is: %i" % len(set_collection)
 
 
 def main():
     parser = argparse.ArgumentParser(
-            description="Script for calculating the distance from starting position in hexagonal grid"
+            description="Script for calculating total number of groups connected to the program with id {0}"
                         "Execute the script by typing python hex_path.py --name file_name",
         )
     parser.add_argument('--name', help='Name of a file to open', nargs='*')
     args = parser.parse_args()
-    # Open a file
-    follow_the_path(args.name)
+    interconnected_groups(args.name)
 
 
 if __name__ == "__main__":
